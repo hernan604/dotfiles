33f2513c55a5cb1ff5b73dc6853bd89b98a659d8
Hernan Lopes <hernanlopes@gmail.com>
2015-04-15 20:41:00 +0000

    added initialization file

---
+++ init.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+PWD=`pwd`
+IGNORE_FILES="README"
+FILES=$(git ls-files)
+for f in $FILES
+    do
+        echo $f
+        if [ ! -f $HOME/$f ]
+            then
+            [[ $IGNORE_FILES =~ $f ]] || `ln -s $PWD/$f $HOME/$f`
+        fi
+    done
