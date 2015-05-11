cda2e916158b3422496854eb0aaeea0304c35a1a
Hernan <hernanlopes@gmail.com>
2015-05-11 14:52:49 +0000

    dotfiles update

--- init.sh
+++ init.sh
@@ -1,15 +1,28 @@
 #!/usr/bin/env bash
 
 PWD=`pwd`
-IGNORE_FILES="README init.sh .gitignore "
-FILES=$(git ls-files)
-for f in $FILES
+##FILES=$(git ls-files)
+for f in {".bashrc",".dataprinter",".mc",".screenrc",".vimrc",".Xresources"}
     do
-        echo $f
+        echo "Working on: $f"
         if [ ! -e $HOME/$f ]
             then
-            [[  $f =~ $IGNORE_FILES ]] || `ln -s $PWD/$f $HOME/$f`
-        else
-            echo "$HOME/$f already exists. not overwriting"
+            `ln -s $PWD/$f $HOME/$f`
         fi
     done
+
+# Vim color schemes
+if [ ! -e $HOME/.vim/colors ]
+then
+    if [ ! -e $HOME/.vim/ ]
+    then
+        mkdir $HOME/.vim
+    fi
+        ln -s "$HOME/dotfiles/vim-themes" "$HOME/.vim/colors"
+fi
+
+# Fonts
+./install_gohufont.sh
+
+echo "updating X-resources"
+xrdb -merge "$HOME/.Xresources"
