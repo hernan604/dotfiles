fda0f13f2c57c928f61af1a8e0cf682501f21d91
Hernan Lopes <hernanlopes@gmail.com>
2015-07-22 10:37:25 -0300

    delete gohufont.tar.gz file after installation is complete

--- install_gohufont.sh
+++ install_gohufont.sh
@@ -2,8 +2,10 @@
 if [ ! -e $HOME/.fonts/gohufont-2.0 ]
     then
 echo "Installing gohufont"
-wget http://font.gohu.org/gohufont-2.0.tar.gz
+gohufont_file="gohufont-2.0.tar.gz"
+wget http://font.gohu.org/$gohufont_file
 tar xvf gohufont-2.0.tar.gz
+rm gohufont_file
 cd gohufont-2.0
 for i in *.gz; do gunzip $i ; done; 
 mv ../gohufont-2.0 $HOME/.fonts
