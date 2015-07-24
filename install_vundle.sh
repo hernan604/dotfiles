f41883e64bb05729e67f5ba8096f4082205cd99c
Hernan Lopes <hernanlopes@gmail.com>
2015-07-24 11:30:50 -0300

    reorganizing app files strucute

--- install_vundle.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [ ! -e $HOME/.vim/bundle/Vundle.vim ]
-    then
-    echo "Installing Vundle"
-    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-fi
