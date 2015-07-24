1635802e84afcd92ba36ec5cd245331cecc9e6d7
Hernan Lopes <hernanlopes@gmail.com>
2015-07-24 11:30:27 -0300

    reorganized vim files

---
+++ vim/install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+echo "Installing vim"
+# Vim color schemes
+if [ ! -e $HOME/.vim/colors ]
+then
+    echo "=> Creating ~/.vim/colors"
+    if [ ! -e $HOME/.vim/ ]
+    then
+        mkdir $HOME/.vim
+    fi
+        ln -s "$HOME/dotfiles/vim/colors" "$HOME/.vim/colors"
+fi
+
+source $PWD/vim/vundle/install.sh
