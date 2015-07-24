1b943afc848c9e382bdf4d6ade28211dcd47f77d
Hernan Lopes <hernanlopes@gmail.com>
2015-07-24 10:42:26 -0300

    updated ubuntu packages

--- packages/ubuntu/install.sh
+++ packages/ubuntu/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env
 
 echo "Installing common ubuntu packages"
-cmd="sudo apt-get install -y build-essential i3 i3lock i3status i3-wm suckless-tools mc chromium-browser htop tmux screen exuberant-ctags"
+cmd="sudo apt-get install -y build-essential i3 i3lock i3status i3-wm suckless-tools mc chromium-browser htop tmux screen exuberant-ctags tree"
 echo $cmd
 $cmd
