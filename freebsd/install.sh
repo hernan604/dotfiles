cb854c7d6bd882eb904e9a31af971b620a3f0b2d
Hernan Lopes <hernanlopes@gmail.com>
2024-05-16 18:15:04 -0300

    some freebsd related updates

--- freebsd/install.sh
+++ freebsd/install.sh
@@ -8,23 +8,20 @@ sudo pw group mod firefox_users -m admin
 sudo pw group mod firefox_users -m administrator
 
 sudo pkg install -y     \
+nvramtool               \
 bash                    \
 beadm                   \
 bhyve-firmware          \
 dmenu                   \
 drm-515-kmod            \
-drm-kmod                \
 firefox                 \
 fusefs-sshfs            \
 git                     \
 gohufont                \
 grub2-bhyve             \
-i3                      \
-i3lock                  \
 slock                   \
 mc                      \
 rexima                  \
-tabbed                  \
 tigervnc-viewer         \
 vim                     \
 vm-bhyve                \
@@ -35,6 +32,7 @@ xf86-input-synaptics    \
 xorg                    \
 xterm                   \
 acpi_call               \
+hwstat                  \
 zsh
 
 # clone antigen for zsh
@@ -89,3 +87,40 @@ sudo cp -i /tmp/wpa_supplicant.conf /usr/local/etc/wifibox/wpa_supplicant/wpa_su
 sudo chown root:wheel /usr/local/etc/wifibox/wpa_supplicant/wpa_supplicant.conf
 sudo chmod 640 /usr/local/etc/wifibox/wpa_supplicant/wpa_supplicant.conf 
 rm /tmp/wpa_supplicant.conf
+
+
+
+####################################################################
+#
+# set battery charging thresholds on laptop:
+# FROM: https://forums.freebsd.org/threads/how-to-set-battery-charging-thresholds-on-laptop.79807/
+# - check that battery is charging:
+#   acpiconf -i0 | grep State
+#   State:                   charging   <---- means battery is charging.. even when its 100%. that will damage the battery. so we want to avoid with this trick
+# - run the command below to list dev.acpi_ settings.
+# - find the handle ie: sysctldev.acpi_ec.0.%location: handle=\_SB_.PCI0.LPCB.EC0_
+#   sysctl -a | grep dev.acpi_
+# - then, use acpi_call -v -p '\$____REPLACE_WITH_HANDLE_HERE____.HKEY.BCSS' -i 95
+# NOTE: BCSS is the percentage to STOP charging
+# NOTE: BCCS is the percentage to START charging
+# ➜  ~ acpi_call -v -p '\_SB_.AC__.HKEY.BCSS' -i 95
+# acpi_callPath: \_SB_.AC__.HKEY.BCSS
+# Number of arguments: 1
+# Argument 1 type: Integer
+# Argument 1 value: 95
+# Status: 5
+# Result: Unknown object type '0'
+# ➜  ~ acpi_call -v -p '\_SB_.AC__.HKEY.BCCS' -i 80
+# acpi_callPath: \_SB_.AC__.HKEY.BCCS
+# Number of arguments: 1
+# Argument 1 type: Integer
+# Argument 1 value: 80
+# Status: 5
+# Result: Unknown object type '0'
+# ➜  ~ acpiconf -i0 | grep State
+# acpiconfState:                   high  <----- now, its not charging anymore, it says its high. even if it is at 96%
+
+
+#############################################
+# Disable intel me on freebsd:
+# sudo nvramtool -w me_state=Disable # disable intel me
