bc85722e24ee2ceb7654147c22fc0cb7b64adcec
Hernan Lopes <hernanlopes@gmail.com>
2024-06-01 19:35:39 -0300

    add movestack patch

--- dwm-6.3/config.h
+++ dwm-6.3/config.h
@@ -1,4 +1,5 @@
 /* See LICENSE file for copyright and license details. */
+#include "movestack.c"
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
@@ -71,6 +72,8 @@ static Key keys[] = {
   { MODKEY,              XK_b,      togglebar,      {0} },
   { MODKEY,              XK_Left,   focusstack,     {.i = +1 } },
   { MODKEY,              XK_Right,  focusstack,     {.i = -1 } },
+  { MODKEY|ShiftMask,              XK_Left,   movestack,     {.i = +1 } },
+  { MODKEY|ShiftMask,              XK_Right,  movestack,     {.i = -1 } },
   { MODKEY|ShiftMask,    XK_Up,     incnmaster,     {.i = +1 } },
   { MODKEY|ShiftMask,    XK_Down,   incnmaster,     {.i = -1 } },
   { MODKEY|ShiftMask,    XK_h,      setmfact,       {.f = -0.05} },
